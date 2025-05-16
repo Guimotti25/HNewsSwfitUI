a6c77fd1ef280bc1b1f798102622eacd45d7c586
Motti <mottiguilherme@gmail.com>
2025-05-16 11:13:36 -0300

    Initial Commit

---
+++ hacker news/hacker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacker_newsApp.swift
+//  hacker news
+//
+//  Created by Guilherme Motti on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct hacker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
